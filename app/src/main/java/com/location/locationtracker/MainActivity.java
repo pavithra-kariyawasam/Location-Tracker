dec022f87f680d7a85e89d8f9e4bcbdf800e7bfe
pavithra-kariyawasam <pavithrakariyawasam123@gmail.com>
2020-01-08 00:58:42 +0530

    Created App

---
+++ app/src/main/java/com/location/locationtracker/MainActivity.java
@@ -0,0 +1,14 @@
+package com.location.locationtracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
